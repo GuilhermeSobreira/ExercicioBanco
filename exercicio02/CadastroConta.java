8c6c2d342f0d82d59e92c15715796bd9ba3595a1
Guilherme Sobreira da Silva <111311934+GuilhermeSobreira@users.noreply.github.com>
2024-03-19 16:30:50 -0300

    Add files via upload

---
+++ exercicio02/CadastroConta.java
@@ -0,0 +1,46 @@
+package exercicio02;
+
+public class CadastroConta {
+    Conta[] conta;
+    int numContas;
+
+    public CadastroConta() {
+        conta = new Conta[3];
+        numContas = 0;
+    }
+
+    public void adicionarConta(String nome, double valor) {
+        if (numContas < conta.length) {
+            conta[numContas] = new Conta(nome, valor);
+            numContas++;
+            System.out.println("Conta adicionada com sucesso!\n");
+        } else {
+            System.out.println("\nLimite de contas atingido!\n");
+        }
+    }
+
+    public void listarContas() {
+        if (numContas == 0) {
+            System.out.println("\nNenhuma conta cadastrada!\n");
+        } else {
+            System.out.println("\nContas cadastradas: ");
+            for (int i = 0; i < numContas; i++) {
+                System.out.println("\nNome: " + conta[i].getNome() + "\nValor: \n" + conta[i].getValor());
+            }
+        }
+    }
+
+    public void procurarConta(String nome) {
+        boolean encontrada = false;
+        for (int i = 0; i < numContas; i++) {
+            if (conta[i].getNome().equalsIgnoreCase(nome)) {
+                System.out.println("\nConta encontrada! \n\nNome: " + conta[i].getNome() + "\nValor: " + conta[i].getValor());
+                encontrada = true;
+                break;
+            }
+        }
+        if (!encontrada) {
+            System.out.println("\nConta não encontrada!\n");
+        }
+    }
+}
